4641d2022ebae120f9f2a39cd4cb6c740bb89348
ladiesman218 <duncej@gmail.com>
2022-03-01 20:27:15 +0800

    Initial Commit

---
+++ My Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Notes
+//
+//  Created by Lei Gao on 2022/M/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
